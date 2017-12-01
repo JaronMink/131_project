ff211275446a12b6f7d37d4d2efb16e7a5e28e00
jmink <jmink@novacoast.com>
2017-11-30 22:29:14 -0800

    Made initial client code using TCP echo client code

---
+++ client.py
@@ -0,0 +1,32 @@
+import asyncio
+
+
+async def tcp_echo_client(message, loop):
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8888,
+                                                   loop=loop)
+
+    print('Send: %r' % message)
+    writer.write(message.encode())
+
+    data = await reader.read(100)
+    print('Received: %r' % data.decode())
+
+    print('Close the socket')
+    writer.close()
+
+
+def main():
+
+    loop = asyncio.get_event_loop()
+    print("send message below")
+    try:
+        while True:
+            message = input(">")
+            loop.run_until_complete(tcp_echo_client(message, loop))
+    except KeyboardInterrupt:
+        pass
+    loop.close()
+
+
+if __name__ == '__main__':
+    main()
