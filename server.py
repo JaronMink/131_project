41d48e7e86eed05e822125f6c59691a907a279c0
jmink <jmink@novacoast.com>
2017-11-30 22:22:38 -0800

    Made initial server code using TCP echo server code

--- server.py
+++ server.py
@@ -2,16 +2,30 @@ import sys
 import asyncio
 
 
+async def handle_client_msg(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    print('Recieved: %s' % message)
 
-
-
-
-
-
+    writer.close()
 
 def main():
-    while True:
-        x = 5
+    #while True\\
+        #x = 5
+    #print('hi')
+    loop = asyncio.get_event_loop()
+    coroutine = asyncio.start_server(handle_client_msg, '127.0.0.1', 8888, loop=loop)
+    server = loop.run_until_complete(coroutine)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    #close socket
+    server.close()
+    loop.run_until_complete(server.wait_close())
+    loop.close()
 
 if __name__ == '__main__':
     main()
